7e210e2917587446ea290f685c74233b26a8522e
NoureddineMaher <nmaher@rubicon.ma>
2024-05-15 01:24:37 +0100

    init committ

---
+++ frontend/src/views/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../components/home/Header'
+import Slider from '../../components/home/Slider'
+
+const index = () => {
+  return (
+    <>
+      <Header />
+      <Slider />
+    </>
+  )
+}
+
+export default index
